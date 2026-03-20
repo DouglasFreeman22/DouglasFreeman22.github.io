9a0157e17fad9e99e1d9732f457a23f6d696c5a4
DouglasFreeman22 <dougief226@gmail.com>
2026-03-20 17:53:56 +0000

    The start of Pong

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -10,9 +10,30 @@ function runProgram(){
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
+  const KEY = {
+  W: 87,
+  UP: 38,
+  S: 83,
+  DOWN: 40,
+};
   
   // Game Item Objects
-
+  var paddle1 = {
+    padX: 0,
+    padY: 0,
+    padSpeedY: 0
+  }
+   var paddle2 = {
+    padX: 0,
+    padY: 0,
+    padSpeedY: 0
+  }
+  var ball = {
+    ballX: 0,
+    ballY: 0,
+    ballSpeedX: 0,
+    ballSpeedY: 0
+  }
 
   // one-time setup
   let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
@@ -27,21 +48,58 @@ function runProgram(){
   by calling this function and executing the code inside.
   */
   function newFrame() {
-    
-
+    update();
   }
   
   /* 
   Called in response to events.
   */
   function handleEvent(event) {
-
+    handleKeyDown();
   }
 
   ////////////////////////////////////////////////////////////////////////////////
   ////////////////////////// HELPER FUNCTIONS ////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
 
+  function handleKeyDown(event) {
+  // the handleKeyDown function register which key is pressed
+activeKey = event.which;
+  // If a valid direction key is pressed, start the game
+  if (
+    event.which === KEY.UP ||
+    event.which === KEY.DOWN ||
+    event.which === KEY.W ||
+    event.which === KEY.S
+  ) {
+    started = true; // the game starts when the first key is pressed
+    
+    if(event.which === KEY.UP){
+    paddle2.padSpeedY = 1;
+    } else if(event.which === KEY.DOWN){
+    paddle2.padSpeedY = -1;
+    } else if(event.which === KEY.W){
+    paddle1.padSpeedY = 1;
+    } else if(event.which === KEY.S){
+    paddle1.padSpeedY = -1;
+    }
+  }
+}
+
+function update() {
+
+if (started) {
+  moveBall();
+}
+
+if (hasCollidedWithWall()) {
+  endGame();
+}
+
+if (hasHitWall()) {
+  wallCollition();
+}
+}
   
   function endGame() {
     // stop the interval timer
