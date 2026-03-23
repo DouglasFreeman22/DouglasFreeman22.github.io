d79db01284cb6cd35e91f2a9c85077f6b82820e8
DouglasFreeman22 <dougief226@gmail.com>
2026-03-23 01:19:11 +0000

    Fixes I needed to do

--- asd-projects/pong/index.js
+++ asd-projects/pong/index.js
@@ -1,43 +1,44 @@
 /* global $, sessionStorage */
 
 $(document).ready(runProgram); // wait for the HTML / CSS elements of the page to fully load, then execute runProgram()
-  
-function runProgram(){
+
+function runProgram() {
   ////////////////////////////////////////////////////////////////////////////////
   //////////////////////////// SETUP /////////////////////////////////////////////
   ////////////////////////////////////////////////////////////////////////////////
-
+  var started = false;
+  var activeKey;
   // Constant Variables
   const FRAME_RATE = 60;
   const FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
   const KEY = {
-  W: 87,
-  UP: 38,
-  S: 83,
-  DOWN: 40,
-};
-  
+    W: 87,
+    UP: 38,
+    S: 83,
+    DOWN: 40,
+  };
+
   // Game Item Objects
   var paddle1 = {
     padX: 0,
     padY: 0,
-    padSpeedY: 0
-  }
-   var paddle2 = {
+    padSpeedY: 0,
+  };
+  var paddle2 = {
     padX: 0,
     padY: 0,
-    padSpeedY: 0
-  }
+    padSpeedY: 0,
+  };
   var ball = {
     ballX: 0,
     ballY: 0,
     ballSpeedX: 0,
-    ballSpeedY: 0
-  }
+    ballSpeedY: 0,
+  };
 
   // one-time setup
-  let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('eventType', handleEvent);                           // change 'eventType' to the type of event you want to handle
+  let interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL); // execute newFrame every 0.0166 seconds (60 Frames per second)
+  $(document).on("keydown", handleEvent); // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
   ///////////////////////// CORE LOGIC ///////////////////////////////////////////
@@ -50,12 +51,12 @@ function runProgram(){
   function newFrame() {
     update();
   }
-  
+
   /* 
   Called in response to events.
   */
   function handleEvent(event) {
-    handleKeyDown();
+    handleKeyDown(event);
   }
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -63,44 +64,60 @@ function runProgram(){
   ////////////////////////////////////////////////////////////////////////////////
 
   function handleKeyDown(event) {
-  // the handleKeyDown function register which key is pressed
-activeKey = event.which;
-  // If a valid direction key is pressed, start the game
-  if (
-    event.which === KEY.UP ||
-    event.which === KEY.DOWN ||
-    event.which === KEY.W ||
-    event.which === KEY.S
-  ) {
-    started = true; // the game starts when the first key is pressed
-    
-    if(event.which === KEY.UP){
-    paddle2.padSpeedY = 1;
-    } else if(event.which === KEY.DOWN){
-    paddle2.padSpeedY = -1;
-    } else if(event.which === KEY.W){
-    paddle1.padSpeedY = 1;
-    } else if(event.which === KEY.S){
-    paddle1.padSpeedY = -1;
+    // the handleKeyDown function register which key is pressed
+    activeKey = event.which;
+    // If a valid direction key is pressed, start the game
+    if (
+      event.which === KEY.UP ||
+      event.which === KEY.DOWN ||
+      event.which === KEY.W ||
+      event.which === KEY.S
+    ) {
+      started = true; // the game starts when the first key is pressed
+
+      if (event.which === KEY.UP) {
+        paddle2.padSpeedY = -5;
+      } else if (event.which === KEY.DOWN) {
+        paddle2.padSpeedY = 5;
+      } else if (event.which === KEY.W) {
+        paddle1.padSpeedY = -5;
+      } else if (event.which === KEY.S) {
+        paddle1.padSpeedY = 5;
+      }
     }
   }
-}
 
-function update() {
+  function moveBall() {}
 
-if (started) {
-  moveBall();
-}
+  function hasCollidedWithWall() {
+    return false;
+  }
 
-if (hasCollidedWithWall()) {
-  endGame();
-}
+  function hasHitWall() {
+    return false;
+  }
 
-if (hasHitWall()) {
-  wallCollition();
-}
-}
-  
+  function wallCollition() {}
+
+  function update(paddle) {
+    paddle1.padY += paddle1.padSpeedY;
+    $("#paddle1").css("top", paddle1.padY + "px");
+    paddle2.padY += paddle2.padSpeedY;
+    $("#paddle2").css("top", paddle2.padY + "px");
+
+    if (started) {
+      moveBall();
+    }
+
+    if (hasCollidedWithWall()) {
+      endGame();
+    }
+
+    if (hasHitWall()) {
+      wallCollition();
+    }
+  }
+
   function endGame() {
     // stop the interval timer
     clearInterval(interval);
@@ -108,5 +125,4 @@ if (hasHitWall()) {
     // turn off event handlers
     $(document).off();
   }
-  
 }
