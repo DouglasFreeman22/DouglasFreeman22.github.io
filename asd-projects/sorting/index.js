58a44329d1a37ecf003232b9e081c8cf1ccdf43b
DouglasFreeman22 <dougief226@gmail.com>
2026-04-09 17:06:28 +0000

    final sorting project

--- asd-projects/sorting/index.js
+++ asd-projects/sorting/index.js
@@ -30,30 +30,32 @@ async function bubbleSort(array){
 
 // TODO 3: Implement quickSort
 async function quickSort(array, left, right){
-    if (right - left > 0){
-        index = partition(array, left, right)
+    if (right - left < 0){
+        return;
     }
-    else {if(left < index - 1){
-            quickSort(array, left, index - 1);
-        }
-        if(index < right){
-            quickSort(array, index, right);
-        }
-    }
+    let index = await partition(array, left, right)
+    if(left < index - 1){
+        await quickSort(array, left, index - 1);
+      }
+     if(right > index){
+        await quickSort(array, index, right);
+     }
 }
 
 // TODOs 4 & 5: Implement partition
-function partition(array, left, right){
-     pivot = pivot
+async function partition(array, left, right){
+    let pivot = array[Math.floor((right + left) / 2)].value;
   while(left < right){
-    while(array[left] < pivot){
+    while(array[left].value < pivot){
      left++ 
         };
-    while(array[right] > pivot){
+    while(array[right].value > pivot){
      right--
         }
     if(left < right){
-        swap(array[left], array[right])
+        swap(array, left, right)
+        updateCounter(quickCounter);
+        await sleep();
     }
   }
   return left + 1
